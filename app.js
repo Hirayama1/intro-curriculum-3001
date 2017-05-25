3d1da6918f5b6608d15172f95768cb7a00c07eb6
Hirayama1 <atnk@i.softbank.jp>
2017-05-25 08:52:04 +0000

    階上関数の実装

--- app.js
+++ app.js
@@ -7,7 +7,10 @@
  */
 function factorial(n) {
     let result = 1;
-    // TODO このコメントを消して正しく実装してください。
+    let result = 1;
+    for (let i = 1; i <= n; i++){
+        result = result * i;
+    }
     return result;
 }
 
